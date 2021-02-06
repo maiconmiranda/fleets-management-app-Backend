856789aa9c677c48a36080084216755fae7e5cd1
maiconmiranda <cam022021@coderacademy.edu.au>
2021-02-06 14:16:05 +1100

    Fix private methot to get the id in all controllers

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -42,7 +42,7 @@ class IncidentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_incident
-      @incident = current_user.incidents.find(params[:id])
+      @incident = Incidents.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -43,7 +43,7 @@ class VehiclesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vehicle
-      @vehicle = current_user.vehicles.find(params[:id])
+      @vehicle = Vehicles.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
