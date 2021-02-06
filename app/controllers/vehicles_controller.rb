df1fe5895f7c04256e66e197d821b274c908da8e
maiconmiranda <cam022021@coderacademy.edu.au>
2021-02-06 14:39:19 +1100

    change vehicle private method

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -43,7 +43,7 @@ class VehiclesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_vehicle
-      @vehicle = Vehicles.find(params[:id])
+      @vehicle = Vehicle.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
