775f9d48cede65480db9b1a8e27cf8fa5ae404c2
maiconmiranda <cam022021@coderacademy.edu.au>
2021-02-09 20:41:26 +1100

    add url cors frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://fleets-management-backend.herokuapp.com/'
+    origins 'localhost:8080', 'https://fleetsmanagement.netlify.app/'
 
     resource '*',
       headers: :any,
